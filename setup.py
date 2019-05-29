19e286e14580dd088d64186527508fe9547d2d5b
ssoos <ssoos@pixelfederation.com>
2019-05-29 16:29:37 +0200

    added basic examples

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='comfy-bayes',
-    version='0.0.9',
+    version='0.0.10',
     packages=['comfy_bayes'],
     url='https://github.com/eki107/comfy-bayes',
     license='',
