23a71964e9f20dc9bad559eb05808e855af3783b
ssoos <ssoos@pixelfederation.com>
2019-07-26 13:30:48 +0200

    - minor fixes to examples visualizations - plotting functions now accept ax as a parameter for convenient subplots

--- comfy_bayes/conversion_plotting.py
+++ comfy_bayes/conversion_plotting.py
@@ -6,7 +6,9 @@ from seaborn.palettes import color_palette
 cpal = color_palette("Set2")
 
 
-def draw_probability_distributions(test: ConversionTest):
+def draw_probability_distributions(test: ConversionTest, ax=None):
+    ax = ax or plt.gca()
+
     # get beta functions as pmf of our observations
     a_beta_func = test.beta_func_a
     b_beta_func = test.beta_func_b
@@ -15,16 +17,13 @@ def draw_probability_distributions(test: ConversionTest):
     top_x = max(a_beta_func.ppf(0.99999), b_beta_func.ppf(0.99999))
     bottom_x = min(a_beta_func.ppf(0.00001),  b_beta_func.ppf(0.00001))
 
-    # plotting starts here
-    fig, ax = plt.subplots(1, 1)
-
     # get values for our chart
     x = np.linspace(bottom_x, top_x, 1000)
     hdi_a = np.logical_and(a_beta_func.ppf(.025) <= x, x <= a_beta_func.ppf(.975))
     hdi_b = np.logical_and(b_beta_func.ppf(.025) <= x, x <= b_beta_func.ppf(.975))
 
     # plot distribution for group A
-    ax.plot(x, a_beta_func.pdf(x), color=cpal[0], label="A")
+    dplot = ax.plot(x, a_beta_func.pdf(x), color=cpal[0], label="A")
     ax.fill_between(x, y1=0, y2=a_beta_func.pdf(x), where=hdi_a, facecolor=cpal[0], alpha=.2, )
 
     # plot distribution for group B
@@ -39,19 +38,20 @@ def draw_probability_distributions(test: ConversionTest):
     # add legend
     ax.legend()
 
-    plt.show()
+    return dplot
 
 
-def draw_approximate_distribution_of_difference(test, samples=1_000_000, alpha=.05):
+def draw_approximate_distribution_of_difference(test, samples=1_000_000, alpha=.05, ax=None):
+    ax = ax or plt.gca()
+
     i = alpha / 2, (1 - alpha / 2)
 
     data = test.beta_func_b.rvs(samples) - test.beta_func_a.rvs(samples)
 
-    fig, ax = plt.subplots(1, 1)
     x = np.sort(data)
     n = len(x)
     N, bins, patches = ax.hist(x=x, bins=100)
-    ax.axvline(0, lw=4, c='pink')
+    dplot = ax.axvline(0, lw=4, c='pink')
 
     hdi = x[int(i[0] * n) - 1], x[int(i[1] * n) - 1]
 
@@ -65,4 +65,4 @@ def draw_approximate_distribution_of_difference(test, samples=1_000_000, alpha=.
     ax.set_title(f"Approximate distribution off difference B-A (sampled from {samples:,d} draws", loc="right")
     ax.set_xticklabels(["{:,.2%}".format(tick) for tick in ax.get_xticks()])
 
-    plt.show()
+    return dplot
